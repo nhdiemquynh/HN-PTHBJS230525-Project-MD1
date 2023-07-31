9cb78234e3257edfb732b7023e36a647f412202f
nhdiemquynh <nguyenhoangdiemquynh@gmail.com>
2023-07-31 19:55:23 +0900

    3107 commit

---
+++ PRODUCTS/products.js
@@ -0,0 +1,17 @@
+$('.js-show-cart').on('click',function(){
+    $('.js-panel-cart').addClass('show-header-cart');
+});
+
+$('.js-hide-cart').on('click',function(){
+    $('.js-panel-cart').removeClass('show-header-cart');
+});
+
+/*==================================================================
+[ Cart ]*/
+$('.js-show-sidebar').on('click',function(){
+    $('.js-sidebar').addClass('show-sidebar');
+});
+
+$('.js-hide-sidebar').on('click',function(){
+    $('.js-sidebar').removeClass('show-sidebar');
+});
