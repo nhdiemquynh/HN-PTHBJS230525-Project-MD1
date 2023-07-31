78ada33fbf7af7af441ff20a027d103395bf1403
nhdiemquynh <nguyenhoangdiemquynh@gmail.com>
2023-07-31 19:21:29 +0900

    3107 commit

--- SIGN_UP/signup.js
+++ SIGN_UP/signup.js
@@ -1,26 +1,26 @@
-// let registerFormVisible = true;
-//   // Lắng nghe sự kiện click trên phần tử có lớp "message"
-//   const messageLinks = document.querySelectorAll('.message');
-//   messageLinks.forEach(link => {
-//     link.addEventListener('click', function(event) {
-//       event.preventDefault(); // Ngăn chặn chuyển hướng đến đường dẫn href="#"
-//       const registerForm = document.getElementById('register-form');
-//       const loginForm = document.getElementById('login-form');
-//       if (registerFormVisible) {
-//         registerForm.style.display = 'none';
-//         loginForm.style.display = 'block';
-//       } else {
-//         registerForm.style.display = 'block';
-//         loginForm.style.display = 'none';
-//       }
-//       // Chuyển đổi trạng thái hiện tại của biểu mẫu
-//       registerFormVisible = !registerFormVisible;
-//     });
-//   });
+let registerFormVisible = true;
+  // Lắng nghe sự kiện click trên phần tử có lớp "message"
+  const messageLinks = document.querySelectorAll('.message');
+  messageLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault(); // Ngăn chặn chuyển hướng đến đường dẫn href="#"
+      const registerForm = document.getElementById('register-form');
+      const loginForm = document.getElementById('login-form');
+      if (registerFormVisible) {
+        registerForm.style.display = 'none';
+        loginForm.style.display = 'block';
+      } else {
+        registerForm.style.display = 'block';
+        loginForm.style.display = 'none';
+      }
+      // Chuyển đổi trạng thái hiện tại của biểu mẫu
+      registerFormVisible = !registerFormVisible;
+    });
+  });
 
-$('.signup-button #signup-btn').click(function(){
-  $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});
+// $('.signup-button #signup-btn').click(function(){
+//   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+// });
   // localStorage.clear()
   // Đăng ký
   function handleSubmit(event) {
